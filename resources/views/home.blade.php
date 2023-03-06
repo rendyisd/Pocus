44d5f0eddf819df2a195523eb03ae41a99d4075f
Rendy I <risdwanta35@gmail.com>
2023-03-06 11:25:52 +0700

    edit cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,25 +18,29 @@
                 Tools
             </h1>
             <div class="row d-flex justify-content-center align-content-center flex-column flex-sm-row">
-                <div class="card col-lg-4 text-center mx-3 mb-4 p-0" id="card-tools-1">
+                <div class="card col-lg-4 text-center mx-3 mb-5 p-0" id="card-tools-1">
                     <a href="#" class="link-overlay position-absolute d-block top-0 left-0 w-100 h-100"></a>
                     <div class="d-flex justify-content-center align-items-center w-100 h-75">
                         <img class="card-img-top" src="{{ asset('images/home-images/clock.png') }}" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h3 class="card-title fw-bold">Pomodoro</h3>
+                        <p class="card-text">
+                            Pomodoro is an effective way to boost productivity and focus through timed intervals and breaks.
+                        </p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                <div class="card col-lg-4 text-center mx-3 mb-4 p-0" id="card-tools-2">
+                <div class="card col-lg-4 text-center mx-3 mb-5 p-0" id="card-tools-2">
                     <a href="#" class="link-overlay position-absolute d-block top-0 left-0 w-100 h-100"></a>
                     <div class="d-flex justify-content-center align-items-center w-100 h-75">
                         <img class="card-img-top" src="{{ asset('images/home-images/cards.png') }}" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h3 class="card-title fw-bold">Flashcards</h3>
+                        <p class="card-text">
+                            Flashcards is a method for memorization and retention of information through active recall.
+                        </p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
@@ -46,8 +50,10 @@
                         <img class="card-img-top" src="{{ asset('images/home-images/books.png') }}" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h3 class="card-title fw-bold">Schedule</h3>
+                        <p class="card-text">
+                            Using a schedule can help you stay organized, prioritize your tasks, reduce stress and procrastination.
+                        </p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
