0deb47638bbe8806e2683675380c94382b87818f
Rendy I <risdwanta35@gmail.com>
2023-03-06 09:31:02 +0700

    main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,53 +2,57 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        {{-- <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="row align-content-center main-text-container mb-5">
+        <div class="col">
+            <h1 class="text-center mb-4 main-text-big">
+                Join thousands of others
+                <br>
+                boosting their productivity
+            </h1>
+            <p class="text-center main-text-sub">Pocus gives you the tools you need to increase your productivity and keep your focus while doing your tasks.</p>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col">
+            <h1 class="text-center main-text-big mb-4">
+                Tools
+            </h1>
+            <div class="row d-flex justify-content-center align-content-center flex-column flex-sm-row">
+                <div class="card col-lg-4 text-center mx-3 mb-4 p-0" id="card-tools-1">
+                    <a href="#" class="link-overlay position-absolute d-block top-0 left-0 w-100 h-100"></a>
+                    <div class="d-flex justify-content-center align-items-center w-100 h-75">
+                        <img class="card-img-top" src="{{ asset('images/home-images/clock.png') }}" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-4 text-center mx-3 mb-4 p-0" id="card-tools-2">
+                    <a href="#" class="link-overlay position-absolute d-block top-0 left-0 w-100 h-100"></a>
+                    <div class="d-flex justify-content-center align-items-center w-100 h-75">
+                        <img class="card-img-top" src="{{ asset('images/home-images/cards.png') }}" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-4 text-center mx-3 p-0" id="card-tools-3">
+                    <a href="#" class="link-overlay position-absolute d-block top-0 left-0 w-100 h-100"></a>
+                    <div class="d-flex justify-content-center align-items-center w-100 h-75">
+                        <img class="card-img-top" src="{{ asset('images/home-images/books.png') }}" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
-        </div> --}}
-        <h1>WHAT THE FUCK</h1>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,69 +15,64 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/sass/home.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-        <div class="bg-image">
-            <div class="bg-image-overlay"></div>
+    <div id="app position-relative">
+        <div class="bg-image"></div>
+    @if(Request::is('/'))
+        <div class="bg-image-gradient"></div>
+        <div class="bg-image-2 sticky-top">
+    @endif
+            
         </div>
-        <nav class="navbar navbar-expand-md navbar-dark">
+        <nav class="navbar navbar-dark">
             <div class="container">
                 <a class="navbar-brand ms-1" href="{{ url('/') }}" style="font-size: 1.8rem">
                     <i class="fa-regular fa-square-check"></i>
                     <span class="ms-1" style="font-weight: 700;">POCUS</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto flex-row">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="btn btn-light me-2" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
-                    </ul>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="btn btn-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="btn btn-light me-2" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="btn btn-light" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                            <div class="dropdown-menu position-absolute dropdown-menu-end" aria-labelledby="navbarDropdown" style="background-color: rgba(0, 0, 0, 0.3);">
+                                <a class="dropdown-item text-light" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown" style="background-color: rgba(0, 0, 0, 0.3);">
-                                    <a class="dropdown-item text-light" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="main-content">
             @yield('content')
         </main>
     </div>
